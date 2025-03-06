f3a991a78837b40c7d2f110a0e47f055b589cf1d
Surya Saddam Saputra <119725916+Kuurianse@users.noreply.github.com>
2025-03-06 21:51:59 +0700

    add section 6 custumer, section 7 explore

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onscroll = function() {
+  let scrollPosition = document.documentElement.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrollPercent = (scrollPosition / scrollHeight) * 100;
+  document.querySelector(".progress-bar").style.width = scrollPercent + "%";
+};
